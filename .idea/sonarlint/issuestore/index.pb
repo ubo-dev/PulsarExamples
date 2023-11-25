5c365708ad9ccc99f0cbda993bca16690136ac76
ubo-dev <19030411048@ybu.edu.tr>
2023-11-25 12:05:30 +0300

    basic example

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+k
+;src/main/java/org/example/javaClient/JavaClientExample.java,b/c/bc6b90c0dfea2ec50882aacb70ffff1c28cae0fc
+f
+6src/main/java/org/example/javaClient/BasicExample.java,b/3/b31ccf5343cabfc72f53ec5c48df78dcda41071f
--- src/main/java/org/example/javaClient/BasicExample.java
+++ src/main/java/org/example/javaClient/BasicExample.java
@@ -1,2 +1,63 @@
-package org.example.javaClient;public class BasicExample {
+package org.example.javaClient;
+
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
+
+public class BasicExample {
+
+    public static void main(String[] args) throws PulsarClientException {
+
+        PulsarClient client = PulsarClient.builder()
+                .serviceUrl("http://127.0.0.1:8080")
+                .build();
+
+        // create producer that will send values as Strings, default is byte[]
+        Producer<String> producer = client.newProducer(Schema.STRING)
+                .topic("hellotopic")
+                .create();
+
+        // create a new message, send it, and block until it is acknowledged
+        producer.newMessage()
+                .key("mykey")
+                .value("myvalue")
+                .sendAsync();       // .sendAsync() wont block until it is acknowledged
+
+        //producer.close();
+        //client.close();
+
+        PulsarClient consumerClient = PulsarClient.builder()
+                .serviceUrl("http://127.0.0.1:8080")
+                .build();
+
+
+        // create a consumer that will receive values as Strings
+        Consumer consumer = consumerClient.newConsumer(Schema.STRING)
+                .topic("mytopic")
+                .subscriptionName("my-subscription")
+                .subscribe();
+
+        while(true) {
+
+            // block and wait until a single message is available
+            Message<String> message = consumer.receive();
+
+            // do something with messsage
+            handleMessage(message);
+
+            // acknowledge the message so that it can be deleted by the message broker
+            consumer.acknowledge(message);
+
+            return;
+        }
+    }
+
+    private static void handleMessage(Message<String> message) {
+
+        System.out.println("Received message is: " + new String(message.getData()));
+    }
+
 }
